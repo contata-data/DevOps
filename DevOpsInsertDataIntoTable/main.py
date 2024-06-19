22faf7fce44191bdb914aa26047a681218cf978c
vikask2018 <vikask@contata.in>
2024-06-19 17:52:35 +0530

    DevOps Test123

--- DevOpsInsertDataIntoTable/main.py
+++ DevOpsInsertDataIntoTable/main.py
@@ -35,7 +35,7 @@ def Insert_Data_Test_Poc(request):
             print('Id is blank')
     else:
         print('Id is missing in payload')
-        return 'Id is missing in payload, please re-request with ids1234567', 500
+        return 'Id is missing in payload, please re-request with ids12345678', 500
     
     if "Name" in request_json:
         Name_ = request_json.get("Name")
