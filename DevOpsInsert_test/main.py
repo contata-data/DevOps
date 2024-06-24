415a41c3e0b0f3e1f3dd4556c2d74bff79f8088f
vikask2018 <vikask@contata.in>
2024-06-24 17:32:03 +0530

    Multiple Env

--- DevOpsInsert_test/main.py
+++ DevOpsInsert_test/main.py
@@ -35,7 +35,7 @@ def Insert_Data_test(request):
             print('Id is blank')
     else:
         print('Id is missing in payload')
-        return 'Id is missing in payload, please re-request with ids1234567890', 500
+        return 'Id is missing in payload, please re-request with ids1234', 500
     
     if "Name" in request_json:
         Name_ = request_json.get("Name")
