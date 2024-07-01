e4916c8324ba8fdfea61fc0ff8d6a04e11590d35
vikask2018 <vikask@contata.in>
2024-07-01 10:57:28 +0530

    Merge 01-07-2024

--- src/DevOpsInsert_test/main.py
+++ src/DevOpsInsert_test/main.py
@@ -46,7 +46,7 @@ def DevOpsInsert_test(request):
     else:
         print("Id is missing in payload")
 
-        return "Id is missing in payload, please re-request with ID_1234", 500
+        return "Id is missing in payload, please re-request with ID_1234 01072024", 500
 
     if "Name" in request_json:
         Name_ = request_json.get("Name")
