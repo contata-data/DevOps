7d52e7f754854adb6848c191751244100f59e394
vikask2018 <vikask@contata.in>
2024-07-10 10:57:54 +0530

    test2

--- src/DevOpsInsert_test_Bucket/main.py
+++ src/DevOpsInsert_test_Bucket/main.py
@@ -45,7 +45,7 @@ def DevOpsInsert_test_Bucket(request):
             print("Id is blank")
     else:
         print("Id is missing in payload")
-        return "Id is missing in payload, please re-request with ID Bucke1", 500
+        return "Id is missing in payload, please re-request with ID Bucke2", 500
     
     if "Name" in request_json:
         Name_ = request_json.get("Name")
