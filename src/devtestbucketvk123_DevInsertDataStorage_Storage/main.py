c6a4d60ab935eef1678312e3728c3e39b485055a
vikask2018 <vikask@contata.in>
2024-07-12 10:42:47 +0530

    storage1

--- src/devtestbucketvk123_DevInsertDataStorage_Storage/main.py
+++ src/devtestbucketvk123_DevInsertDataStorage_Storage/main.py
@@ -45,7 +45,7 @@ def DevInsertDataStorage(request):
             print("Id is blank")
     else:
         print("Id is missing in payload")
-        return "Id is missing in payload, please re-request with ID Pub/Sub1", 500
+        return "Id is missing in payload, please re-request with ID Pub/Sub111", 500
 
     if "Name" in request_json:
         Name_ = request_json.get("Name")
