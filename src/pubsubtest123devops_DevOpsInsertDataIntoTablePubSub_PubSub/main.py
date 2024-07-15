29d851f2aaad2c6bbaeae51252c9799dd753c821
vikask2018 <vikask@contata.in>
2024-07-15 16:45:41 +0530

    test

--- src/pubsubtest123devops_DevOpsInsertDataIntoTablePubSub_PubSub/main.py
+++ src/pubsubtest123devops_DevOpsInsertDataIntoTablePubSub_PubSub/main.py
@@ -45,7 +45,7 @@ def DevOpsInsertDataIntoTableStoragePubSub(request):
             print("Id is blank")
     else:
         print("Id is missing in payload1")
-        return "Id is missing in payload, please re-request with id 01072024 Commit 21111", 500
+        return "Id is missing in payload, please re-request with id 01072024 Commit 211111", 500
 
     if "Name" in request_json:
         Name_ = request_json.get("Name")
