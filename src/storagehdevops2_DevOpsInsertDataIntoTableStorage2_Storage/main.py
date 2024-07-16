80f4d779428a3b63d33f6dade99d5e28e3efe005
vikask2018 <vikask@contata.in>
2024-07-16 14:13:24 +0530

    main

--- src/storagehdevops2_DevOpsInsertDataIntoTableStorage2_Storage/main.py
+++ src/storagehdevops2_DevOpsInsertDataIntoTableStorage2_Storage/main.py
@@ -45,7 +45,7 @@ def DevOpsInsertDataIntoTableStorage2(request):
             print("Id is blank")
     else:
         print("Id is missing in payload1")
-        return "Id is missing in payload, please re-request with id 01072024 Commit 1234", 500
+        return "Id is missing in payload, please re-request with id 01072024 Commit 123", 500
 
     if "Name" in request_json:
         Name_ = request_json.get("Name")
