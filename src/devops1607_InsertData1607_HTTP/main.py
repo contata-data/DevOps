f0f4a466454b782013457c426a86c28b5971446b
vikask2018 <vikask@contata.in>
2024-07-16 15:02:11 +0530

    multiple cloud function deploy

--- src/devops1607_InsertData1607_HTTP/main.py
+++ src/devops1607_InsertData1607_HTTP/main.py
@@ -45,7 +45,7 @@ def InsertData1607(request):
             print("Id is blank")
     else:
         print("Id is missing in payload1")
-        return "Id is missing in payload, please re-request with id 01072024 Commit 2", 500
+        return "Id is missing in payload, please re-request with id 01072024 Commit 3", 500
 
     if "Name" in request_json:
         Name_ = request_json.get("Name")
