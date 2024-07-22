f1bf90ea3dad990ecc4d53a26ba193b02eb6cc5b
vikask2018 <vikask@contata.in>
2024-07-22 16:55:17 +0530

    BigQuery

---
+++ srcSQL/CreateDevOpsTestTable.sql
@@ -0,0 +1,6 @@
+create table relevate-dev-403605.RelevateSystemStaging.DevOpsTest
+(
+  Id INT64
+  ,Name STRING
+);
+Insert into relevate-dev-403605.RelevateSystemStaging.DevOpsTest values(1,'Vikas Kumar')
