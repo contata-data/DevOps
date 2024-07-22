51f8d3e64f093b64d8c2ab818a3a60006d4fb746
vikask2018 <vikask@contata.in>
2024-07-22 14:04:58 +0530

    Test1

--- src/devops2207_InsertDataStorage2207_Storage/main.py
+++ src/devops2207_InsertDataStorage2207_Storage/main.py
@@ -45,7 +45,7 @@ def InsertDataStorage2207(request):
             print("Id is blank")
     else:
         print("Id is missing in payload")
-        return "Id is missing in payload, please re-request with id Storage12", 500
+        return "Id is missing in payload, please re-request with id Storage123", 500
     
     if "Name" in request_json:
         Name_ = request_json.get("Name")
