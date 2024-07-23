a4f4ada0d399a2d0bd5bcfd2d7375918c3efa473
vikask2018 <vikask@contata.in>
2024-07-23 19:09:43 +0530

    BigQuery

--- srcSQL/CreateDevOpsTestTable_Proc.sql
+++ srcSQL/CreateDevOpsTestTable_Proc.sql
@@ -1,4 +1,7 @@
 CREATE OR REPLACE PROCEDURE `relevate-dev-403605.RelevateSystem.DevOpsTest_Proc`(id_ int64)
 BEGIN
 select Id, Name  FROM RelevateSystemStaging.DevOpsTest where id=id_ ;
+union all 
+select Id, Name  FROM RelevateSystemStaging.DevOpsTest where id=id_ ;
+
 END; 
