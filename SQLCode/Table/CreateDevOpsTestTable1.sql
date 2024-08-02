52da246f7b6a2d82e0b01a91251288b1a74135c1
vikask2018 <vikask@contata.in>
2024-08-02 17:28:48 +0530

    Test

---
+++ SQLCode/Table/CreateDevOpsTestTable1.sql
@@ -0,0 +1,7 @@
+Drop table if exists relevate-dev-403605.RelevateSystemStaging.DevOpsTest1;
+create table relevate-dev-403605.RelevateSystemStaging.DevOpsTest1
+(
+  Id INT64
+  ,Name STRING
+);
+Insert into relevate-dev-403605.RelevateSystemStaging.DevOpsTest values1(1,'Vikas Kumar Mishra'),(2,'Vikas Kumar'),(3,'Vikas Kumar');
