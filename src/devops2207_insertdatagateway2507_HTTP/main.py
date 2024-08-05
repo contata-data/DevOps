dadc3326962422bfc82a550201d3347dc2c7d283
vikask2018 <vikask@contata.in>
2024-08-05 15:09:08 +0530

    Release Bigquery Test

--- src/devops2207_insertdatagateway2507_HTTP/main.py
+++ src/devops2207_insertdatagateway2507_HTTP/main.py
@@ -28,7 +28,7 @@ def insertdatagateway2507(request):
     )
     request_logging_data = json.dumps(
         {
-            "cloudfunction": "relevate-ai-decile-report-data-insert",
+            "cloudfunction": "relevate-ai-decile-report-data-insert ",
             "ipaddress": f"{ip_address}",
             "payload": request_json,
             "createdon": f"{current_timestamp}",
