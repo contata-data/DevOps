887377e0895fc1420337934ccb0f513d45100543
vikask2018 <vikask@contata.in>
2024-08-06 12:19:03 +0530

    Set DevOps repo

--- src/devops2207_insertdatagateway2507_HTTP/main.py
+++ src/devops2207_insertdatagateway2507_HTTP/main.py
@@ -45,7 +45,7 @@ def insertdatagateway2507(request):
             print("Id is blank")
     else:
         print("Id is missing in payload")
-        return "Id is missing in payload, please re-request with id Storage1234", 500
+        return "Id is missing in payload, please re-request with id Storage1234 ", 500
     
     if "Name" in request_json:
         Name_ = request_json.get("Name")
