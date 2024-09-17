2b0020a98063f52954769b1abf03f74b8193a824
vikask2018 <vikask@contata.in>
2024-09-17 16:24:56 +0530

    main

--- src/devops1309_cfsizeCICD1_HTTP/main.py
+++ src/devops1309_cfsizeCICD1_HTTP/main.py
@@ -45,7 +45,7 @@ def cfsizeCICD1(request):
             print("Id is blank")
     else:
         print("Id is missing in payload")
-        return "Id is missing in payload, please re-request with id Storage 1", 500
+        return "Id is missing in payload, please re-request with id Storage 1 ", 500
     
     if "Name" in request_json:
         Name_ = request_json.get("Name")
