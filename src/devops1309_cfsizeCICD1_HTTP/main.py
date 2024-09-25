87697177a5a722cb849c8aa78c72f9e67c6b3cdd
vikask2018 <vikask@contata.in>
2024-09-25 12:06:51 +0530

    Api test

--- src/devops1309_cfsizeCICD1_HTTP/main.py
+++ src/devops1309_cfsizeCICD1_HTTP/main.py
@@ -28,7 +28,7 @@ def cfsizeCICD1(request):
     )
     request_logging_data = json.dumps(
         {
-            "cloudfunction": "relevate-ai-decile-report-data-insert11245678900",
+            "cloudfunction": "relevate-ai-decile-report-data-insert1124567890011",
             "ipaddress": f"{ip_address}",
             "payload": request_json,
             "createdon": f"{current_timestamp}",
