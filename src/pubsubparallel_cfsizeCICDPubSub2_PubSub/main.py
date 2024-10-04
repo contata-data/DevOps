4eb308ce02ae365c3ddb6a223ecbe8fca9d3ca91
vikask2018 <vikask@contata.in>
2024-10-04 18:19:29 +0530

    test

--- src/pubsubparallel_cfsizeCICDPubSub2_PubSub/main.py
+++ src/pubsubparallel_cfsizeCICDPubSub2_PubSub/main.py
@@ -28,7 +28,7 @@ def cfsizeCICDPubSub2(request):
     )
     request_logging_data = json.dumps(
         {
-            "cloudfunction": "relevate-ai-decile-report-data-insert11245678900116778_2_2",
+            "cloudfunction": "relevate-ai-decile-report-data-insert11245678900116778_2_3",
             "ipaddress": f"{ip_address}",
             "payload": request_json,
             "createdon": f"{current_timestamp}",
