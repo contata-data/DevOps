d7507ffb10ba6f261d284373d628cb9afa1a3d4e
vikask2018 <vikask@contata.in>
2025-07-03 16:41:04 +0530

    skiptest5

--- src/devops1309_configjsonfile1_HTTP/main.py
+++ src/devops1309_configjsonfile1_HTTP/main.py
@@ -28,7 +28,7 @@ def configjsontest_test_json_1(request):
     )
     request_logging_data = json.dumps(
         {
-            "cloudfunction": "relevate-ai-decile-report-data-insert1124567890011",
+            "cloudfunction": "relevate-ai-decile-report-data-insert112456789",
             "ipaddress": f"{ip_address}",
             "payload": request_json,
             "createdon": f"{current_timestamp}",
